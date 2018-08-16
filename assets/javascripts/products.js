e24a00619460073a6c3681c4e2efd49101d96352
Jola M <majo7704@gmail.com>
2018-08-16 19:33:57 +0200

    Minor changes

--- assets/javascripts/products.js
+++ assets/javascripts/products.js
@@ -5,6 +5,17 @@ let hat = {
   image: '<imgHref>'
 };
 typeof hat.toString();
-
-
 console.log(hat);
+
+function Hat(name, price, color, image) {
+  this.name = name;
+  this.price = price;
+  this.color = color;
+  this.image = image;
+  Hat.prototype.toString = function () {
+    console.log(hat.toString());
+  }
+};
+
+var redHat = new Hat("Baseball cap", 11.99, "red", "image");
+console.log(redHat);
